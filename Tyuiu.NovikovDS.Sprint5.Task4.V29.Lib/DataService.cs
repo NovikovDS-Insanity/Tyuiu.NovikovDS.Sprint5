e1e6fc747c3b6073ea1a7cea3cad1c38d2832b65
NovikovDS-Insanity <Tyuiu.NovikovDS@outlook.com>
2024-10-24 16:35:56 +0300

    Task4

---
+++ Tyuiu.NovikovDS.Sprint5.Task4.V29.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.NovikovDS.Sprint5.Task4.V29.Lib
+{
+    public class DataService : ISprint5Task4V29
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double x = Convert.ToDouble(File.ReadAllText(path));
+            double y;
+
+            if (x == 0) y = -1;
+            else y = Math.Round((x / (2 * x) + Math.Sin(Math.Pow(x, 2))), 3);
+
+            return y;
+        }
+    }
+}
---
+++ Tyuiu.NovikovDS.Sprint5.Task4.V29/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.NovikovDS.Sprint5.Task4.V29.Lib;
+
+DataService ds = new();
+
+Console.Title = "Спринт #5 | Выполнил: Новиков Д. С. | РППб-24-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #7                                                              *");
+Console.WriteLine("* Выполнил: Новиков Д. С. | РППб-24-1                                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дано выражение , вычислить его значение при x = 2, результат            *");
+Console.WriteLine("* сохранить в бинарный файл OutPutFileTask3.bin и вывести                 *");
+Console.WriteLine("* на консоль. Округлить до трёх знаков после запятой.                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask4V29.txt");
+
+Console.WriteLine("* Путь файла: " + path);
+Console.WriteLine("* X = " + File.ReadAllText(path));
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double y = ds.LoadFromDataFile(path);
+
+Console.WriteLine("* Y = " + y);
+
+Console.WriteLine("***************************************************************************");
+Console.ReadLine();
+
