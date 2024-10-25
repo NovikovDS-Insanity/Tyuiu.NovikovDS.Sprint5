c3b831ae6ad213044ee67ce2f30d2b1a739ae77a
NovikovDS-Insanity <Tyuiu.NovikovDS@outlook.com>
2024-10-25 08:43:13 +0300

    Task5

---
+++ Tyuiu.NovikovDS.Sprint5.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.NovikovDS.Sprint5.Task5.V3.Lib
+{
+    public class DataService : ISprint5Task5V3
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double sum = 0;
+
+            string str = File.ReadAllText(path);
+            string[] chisla = str.Split(',');
+
+            for (int i = 0; i < chisla.Length; i++)
+            {
+                double temp = Convert.ToDouble(chisla[i]);
+                sum += temp;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.NovikovDS.Sprint5.Task5.V3/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.NovikovDS.Sprint5.Task5.V3.Lib;
+
+DataService ds = new();
+
+Console.Title = "Спринт #5 | Выполнил: Новиков Д. С. | РППб-24-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #7                                                              *");
+Console.WriteLine("* Выполнил: Новиков Д. С. | РППб-24-1                                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дано выражение , вычислить его значение при x = 2, результат            *");
+Console.WriteLine("* сохранить в бинарный файл OutPutFileTask3.bin и вывести                 *");
+Console.WriteLine("* на консоль. Округлить до трёх знаков после запятой.                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask5V3.txt");
+
+Console.WriteLine("* Путь файла: " + path);
+Console.WriteLine("* Набор чисел: " + File.ReadAllText(path));
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double y = ds.LoadFromDataFile(path);
+
+Console.WriteLine("* Сумма всех чисел: " + y);
+
+Console.WriteLine("***************************************************************************");
+Console.ReadLine();
+
