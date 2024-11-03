358a8b116a6c4fe55ea589bd380d91218d45d79f
NovikovDS-Insanity <Tyuiu.NovikovDS@outlook.com>
2024-11-03 22:37:34 +0300

    Task6

--- Tyuiu.NovikovDS.Sprint5.Task6.V10.Lib/DataService.cs
+++ Tyuiu.NovikovDS.Sprint5.Task6.V10.Lib/DataService.cs
@@ -9,11 +9,13 @@ namespace Tyuiu.NovikovDS.Sprint5.Task6.V10.Lib
             int sum = 0;
 
             string str = File.ReadAllText(path);
-            string[] chisla = str.Split(' ');
+            string[] chisla = str.Split(", ");
 
             for (int i = 0; i < chisla.Length; i++)
             {
+                if (i == chisla.Length - 1) chisla[i] = chisla[i].Remove(chisla[i].Length-1);
                 if (chisla[i].Length == 4) sum++;
+                Console.WriteLine(chisla[i]);
             }
 
             return sum;
